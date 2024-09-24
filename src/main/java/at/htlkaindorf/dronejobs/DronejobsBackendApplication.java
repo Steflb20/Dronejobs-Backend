0eeaa6eba831df8bc69ad7764368fb769fce6832
Sternad Florian <steflb20@htl-kaindorf.at>
2024-09-24 13:43:08 +0200

    [INIT] initial commit

---
+++ src/main/java/at/htlkaindorf/dronejobs/DronejobsBackendApplication.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.dronejobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronejobsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronejobsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/htlkaindorf/dronejobs/DronejobsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.dronejobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronejobsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
